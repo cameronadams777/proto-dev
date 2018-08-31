ce13b467370e6c7b3d3c90ffcc47b9a8cc7cd6f7
cameronadams777 <cameronadams777@gmail.com>
2018-08-31 00:18:30 -0500

    fix CVE-2018-15685 by increasing electron version

---
+++ src/components/the-global-modal.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <PreferencesModal v-if="preferencesModalShown"/>
+  </div>
+</template>
+
+<script>
+import { interfaceGetters } from '../store/helpers'
+import PreferencesModal from './preferences-modal'
+export default {
+  components: {
+    PreferencesModal
+  },
+  computed: {
+    ...interfaceGetters
+  }
+}
+</script>
