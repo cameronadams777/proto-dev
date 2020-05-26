1b141153e4dd4f28f1e932cac3a68a623e7d3d7c
Cameron Adams <cameronadams777@gmail.com>
2020-05-26 15:24:41 -0500

    develop login and register UI

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,3 +1,44 @@
 <template>
-  <div></div>
+  <q-page class="login-page flex flex-center">
+    <q-card class="login-page__form-card">
+      <span class="login-page__form-title">Proto Dev</span>
+      <q-input filled v-model="email" label="Email" class="login-page__form-input"/>
+      <q-input filled v-model="password" label="Password" class="login-page__form-input"/>
+      <div class="flex">
+        <q-btn color="black" label="Login" />
+        <q-btn label="Register" flat @click="$router.push('/register')"/>
+      </div>
+    </q-card>
+  </q-page>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.login-page {
+  &__form-card {
+    padding: 2rem;
+    width: 25%;
+  }
+
+  &__form-title {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+    font-size: 2rem;
+  }
+
+  &__form-input {
+    margin-bottom: 1rem;
+  }
+}
+</style>
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,3 +1,46 @@
 <template>
-  <div></div>
+  <q-page class="register-page flex flex-center">
+    <q-card class="register-page__form-card">
+      <span class="register-page__form-title">Proto Dev</span>
+      <q-input filled v-model="email" label="Email" class="register-page__form-input"/>
+      <q-input filled v-model="password" label="Password" class="register-page__form-input"/>
+      <q-input filled v-model="confirmPassword" label="Confirm Password" class="register-page__form-input"/>
+      <div class="flex">
+        <q-btn color="black" label="Register" />
+        <q-btn label="Login" flat @click="$router.push('/login')"/>
+      </div>
+    </q-card>
+  </q-page>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.register-page {
+  &__form-card {
+    padding: 2rem;
+    width: 25%;
+  }
+
+  &__form-title {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+    font-size: 2rem;
+  }
+
+  &__form-input {
+    margin-bottom: 1rem;
+  }
+}
+</style>
