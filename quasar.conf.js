c6b25f17a6202e2c356b49bd96f610d461787f59
Cameron Adams <cameronadams777@gmail.com>
2020-06-07 22:06:15 -0500

    add basic google analytics to application

--- quasar.conf.js
+++ quasar.conf.js
@@ -13,7 +13,7 @@ module.exports = function(/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
-    boot: ["firebase", "vuefire", "vuelidate"],
+    boot: ["firebase", "vuefire", "vuelidate", "vue-gtag"],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: ["app.scss"],
